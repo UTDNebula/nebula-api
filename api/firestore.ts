e6eae17be89b4bde5fa71d7ae2d672e07fd300de
heartgg <a.szumski98@gmail.com>
2021-11-07 13:27:29 -0600

    setup dev environment firestore

---
+++ api/firestore.ts
@@ -0,0 +1,14 @@
+// temporary setup to use firestore in development
+
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const serviceAccount = require('../serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+export default db;
--- api/routes/router.ts
+++ api/routes/router.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import db from '../firestore';
 
 const router = express.Router();
 
