238bb974e12a7212e84ecd9c599613188b64eef7
heartgg <a.szumski98@gmail.com>
2021-11-07 14:45:01 -0600

    added GET /v1/sections/:id endpoint

--- api/routes/router.ts
+++ api/routes/router.ts
@@ -7,4 +7,13 @@ router.route('/').get((req, res) => {
   res.json({ success: true });
 });
 
+router.route('/v1/sections/:id').get(async (req, res) => {
+  const section = await db.collection('sections').doc(req.params.id).get();
+  if (section.exists) {
+    res.status(200).json(section.data());
+  } else {
+    res.status(404).json({ message: 'Section not found by id.' });
+  }
+});
+
 export default router;
