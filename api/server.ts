9720fd2fa508efd86c8ec20d8d857b2db72ac3df
Adam <82891010+heartgg@users.noreply.github.com>
2021-11-11 08:38:11 -0600

    properly import routes
    Co-authored-by: Willie Chalmers III <willie.chalmers@acmutd.co>

--- api/server.ts
+++ api/server.ts
@@ -2,16 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import middlewareController from './middleware/controller';
 
-import * as router from './routes/router';
-import * as sections from './routes/sections';
+import router from './routes/router';
+import sections from './routes/sections';
 
 const app = express();
 
 dotenv.config();
 
 app.use(middlewareController);
-app.use('/', router.default);
-app.use('/v1/sections', sections.default);
+app.use('/', router);
+app.use('/v1/sections', sections);
 
 app.listen(process.env.PORT, () => {
   console.log(`The server has started on port ${process.env.PORT}`);
