21e38ff39a4401517ec207dc902d904ec662428a
Charlie Mahana <35280274+CharlieMahana@users.noreply.github.com>
2022-01-12 20:48:23 -0500

    feat: added course search and course by id endpoints

--- api/routes/router.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.route('/').get((req, res) => {
-  res.json({ success: true });
-});
-
-export default router;
--- api/server.ts
+++ api/server.ts
@@ -2,14 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 
-import router from './routes/router';
+import course from './routes/course';
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   const app = express();
 
-  app.use('/', router);
+  app.use('/course', course);
 
   app.listen(process.env.PORT, () => {
     console.log(`The server has started on port ${process.env.PORT}`);
