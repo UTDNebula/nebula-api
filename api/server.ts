da1d7a464cc4484cd144e060e38d81a0088dbea2
heartgg <a.szumski98@gmail.com>
2022-03-12 12:20:10 -0600

    add query and /:id endpoints to professor

--- api/server.ts
+++ api/server.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 
 import course from './routes/course';
 import degree from './routes/degree';
+import professor from './routes/professor';
 import token from './controllers/token';
 
 dotenv.config();
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
   app.use('/course', course);
   app.use('/degree', degree);
+  app.use('/professor', professor);
 
   app.listen(process.env.PORT, () => {
     console.log(`The server has started on port ${process.env.PORT}`);
