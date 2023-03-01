9ca6a84b0d7940227580562f19d8dd5be0c78966
dlaunikitis <david.launikitis@gmail.com>
2023-02-28 22:13:22 -0600

    Updated ENV handling, CORS, and Critical Dependencies

--- api/configs/setup.go
+++ api/configs/setup.go
@@ -15,7 +15,7 @@ import (
 )
 
 func ConnectDB() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
+	client, err := mongo.NewClient(options.Client().ApplyURI(GetEnvMongoURI()))
 	if err != nil {
 		log.Fatalf("Unable to create MongoDB client: %v", err)
 	}
@@ -37,31 +37,33 @@ func ConnectDB() *mongo.Client {
 	return client
 }
 
-//Client instance
+// Client instance
 var DB *mongo.Client = ConnectDB()
 
-//getting database collections
+// getting database collections
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
 	collection := client.Database("combinedDB").Collection(collectionName)
 	return collection
 }
 
 // Returns *options.FindOptions with a limit and offset applied. Returns error if any
-func GetOptionLimit(query *bson.M, c *gin.Context) (*options.FindOptions, error){
+func GetOptionLimit(query *bson.M, c *gin.Context) (*options.FindOptions, error) {
 	delete(*query, "offset") // removes offset (if present) in query --offset is not field in collections
 
 	// parses offset if included in the query
-	var offset int64;
+	var offset int64
 	var err error
-	
+
+	var limit int64 = GetEnvLimit()
+
 	if c.Query("offset") == "" {
-		offset = 0 	// default value for offset
+		offset = 0 // default value for offset
 	} else {
 		offset, err = strconv.ParseInt(c.Query("offset"), 10, 64)
 		if err != nil {
-			return options.Find().SetSkip(0).SetLimit(Limit), err  // default value for offset
+			return options.Find().SetSkip(0).SetLimit(limit), err // default value for offset
 		}
 	}
 
-	return options.Find().SetSkip(offset).SetLimit(Limit), err
+	return options.Find().SetSkip(offset).SetLimit(limit), err
 }
