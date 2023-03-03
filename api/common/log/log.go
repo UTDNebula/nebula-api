e8ae12e833a978f1c61c80acd9c0e1071c2101a5
Ahad Shabbir <ashabbir@mailbox.org>
2023-03-03 17:59:04 -0600

    added logging middleware

--- api/common/log/log.go
+++ api/common/log/log.go
@@ -8,11 +8,11 @@ import (
 var (
 	// Error should be used for Internal API errors. Any client side error should have a high-level log with Info
 	// or a low level log with Debug.
-	Error = log.New(os.Stderr, "[Error] ", log.Lmsgprefix)
+	Error = log.New(os.Stderr, "[ERR] ", log.Lmsgprefix)
 
 	// Debug should be used for low-level logs, such as database transactions or anything detailing the API logic only.
-	Debug = log.New(os.Stderr, "[Debug] ", log.Lmsgprefix)
+	Debug = log.New(os.Stderr, "[DBG] ", log.Lmsgprefix)
 
 	// Info should be used for logging API requests at the high level.
-	Info = log.New(os.Stdout, "[Info]  ", log.Lmsgprefix)
+	Info = log.New(os.Stdout, "[INF] ", log.Lmsgprefix)
 )
--- api/server.go
+++ api/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/UTDNebula/nebula-api/api/common/log"
 	"github.com/UTDNebula/nebula-api/api/configs"
 	"github.com/UTDNebula/nebula-api/api/routes"
 	"github.com/gin-gonic/gin"
@@ -12,10 +13,13 @@ func main() {
 	configs.ConnectDB()
 
 	// Configure Gin Router
-	router := gin.Default()
+	router := gin.New()
 
 	// Enable CORS
 	router.Use(CORS())
+
+	// Enable Logging
+	router.Use(Log())
 
 	// Connect Routes
 	routes.CourseRoute(router)
@@ -46,6 +50,17 @@ func CORS() gin.HandlerFunc {
 			return
 		}
 
+		c.Next()
+	}
+}
+
+func Log() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Info.Printf("%s %s from %s",
+			c.Request.Method,
+			c.Request.URL.Path,
+			c.Request.Host,
+		)
 		c.Next()
 	}
 }
