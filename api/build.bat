76a7addc796621df2561ce2fb9873267a26302b5
jpahm <20374744+jpahm@users.noreply.github.com>
2023-11-01 14:28:35 -0500

    Improve build.bat

--- api/build.bat
+++ api/build.bat
@@ -5,22 +5,25 @@ set EXEC_NAME=go-api.exe
 
 ::setup
 echo Performing setup...
-go install honnef.co/go/tools/cmd/staticcheck@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest && ^
 go install golang.org/x/tools/cmd/goimports@latest
+if ERRORLEVEL 1 exit /b %ERRORLEVEL% :: fail if error occurred
 echo Setup done!
 echo[
 
 ::checks
 echo Performing checks...
-go mod tidy
-go vet ./... 
-staticcheck ./...
-gofmt -w ./..
+go mod tidy && ^
+go vet ./... && ^
+staticcheck ./... && ^
+gofmt -w ./.. && ^
 goimports -w ./..
+if ERRORLEVEL 1 exit /b %ERRORLEVEL% :: fail if error occurred
 echo Checks done!
 echo[
 
 ::build
 echo Building...
 go build -o %EXEC_NAME% server.go
+if ERRORLEVEL 1 exit /b %ERRORLEVEL% :: fail if error occurred
 echo Build complete!
