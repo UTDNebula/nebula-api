20ecf81070f702f8983c9d997fd687b201de0102
mikehquan19 <hoangphucquan19@gmail.com>
2024-09-21 14:43:00 -0500

    delete the api directory in graphql branch (#220)

--- api/responses/autocomplete_response.go
+++
@@ -1,7 +0,0 @@
-package responses
-
-type AutocompleteResponse struct {
-	Status  int         `json:"status"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-}
--- api/responses/grades_response.go
+++
@@ -1,7 +0,0 @@
-package responses
-
-type GradeResponse struct {
-	Status  int         `json:"status"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-}
