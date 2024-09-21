6605130276f26d9d6a7adb42192596541393469c
mikehquan19 <hoangphucquan19@gmail.com>
2024-09-21 14:42:46 -0500

    delete the graphql directory in develop branch (#221)

--- graphql/graph/model/academic_session.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type AcademicSession struct {
-	Name      string `json:"name"`
-	StartDate string `json:"start_date" bson:"start_date"`
-	EndDate   string `json:"end_date" bson:"end_date"`
-}
