acfb6ff635f474b2d18d2979000f06e3a5c96a60
mikehquan19 <hoangphucquan19@gmail.com>
2024-09-20 22:40:39 -0500

    delete the api directory in graphql branch

--- api/responses/evaluation_response.go
+++
@@ -1,9 +0,0 @@
-package responses
-
-import "github.com/UTDNebula/nebula-api/api/schema"
-
-type EvaluationResponse struct {
-	Status  int               `json:"status"`
-	Message string            `json:"message"`
-	Data    schema.Evaluation `json:"data"`
-}
