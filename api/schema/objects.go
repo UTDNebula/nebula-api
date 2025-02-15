72cf018dd711e880bc183da28615569f71eed114
Tyler Hill <gotyhill@gmail.com>
2025-02-15 00:07:34 -0600

    Add schemas

--- api/schema/objects.go
+++ api/schema/objects.go
@@ -118,6 +118,33 @@ type Event struct {
 	ContactPhoneNumber string             `bson:"contact_phone_number" json:"contact_phone_number"`
 }
 
+// Event hierarchy
+type MultiBuildingEvents[T any] struct {
+	Buildings []SingleBuildingEvents[T] `bson:"buildings" json:"buildings"`
+}
+type SingleBuildingEvents[T any] struct {
+	Building string          `bson:"building" json:"building"`
+	Rooms    []RoomEvents[T] `bson:"rooms" json:"rooms"`
+}
+type RoomEvents[T any] struct {
+	Room   string `bson:"room" json:"room"`
+	Events []T    `bson:"events" json:"events"`
+}
+
+// Event types
+type AstraEvent struct {
+	ActivityName        *string  `bson:"activity_name" json:"activity_name"`
+	MeetingType         *string  `bson:"meeting_type" json:"meeting_type"`
+	StartDate           *string  `bson:"start_date" json:"start_date"`
+	EndDate             *string  `bson:"end_date" json:"end_date"`
+	BuildingCode        *string  `bson:"building_code" json:"building_code"`
+	RoomNumber          *string  `bson:"room_number" json:"room_number"`
+	CurrentState        *string  `bson:"current_state" json:"current_state"`
+	NotAllowedUsageMask *float64 `bson:"not_allowed_usage_mask" json:"not_allowed_usage_mask"`
+	UsageColor          *string  `bson:"usage_color" json:"usage_color"`
+	Capacity            *float64 `bson:"capacity" json:"capacity"`
+}
+
 // 5 Level Likert Item scale for evaluation responses
 type EvaluationResponse int
 
