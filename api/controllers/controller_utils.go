7f909a3474c1d915b131c13be9125031691d1fed
Razeen Rahman <razeen.rahman05@gmail.com>
2025-09-19 00:56:25 -0500

    Create and use generic getQuery across section, course, and professor endpoints

--- api/controllers/controller_utils.go
+++ api/controllers/controller_utils.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/UTDNebula/nebula-api/api/schema"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -14,6 +15,17 @@ import (
 func respond[T any](c *gin.Context, status int, message string, data T) {
 	c.JSON(status, schema.APIResponse[T]{Status: status, Message: message, Data: data})
 }
+// Builds a MongoDB filter from request query parameters for the given schema type T.
+// Automatically responds with HTTP 400 if the parameters are invalid.
+func getQuery[T any](c *gin.Context) (bson.M, error) {
+	q, err := schema.FilterQuery[T](c)
+	if err != nil {
+		respond(c, http.StatusBadRequest, "Invalid query parameters", err.Error())
+		return nil, err
+	}
+	return q, nil
+}
+
 
 // Helper function for logging and responding to a generic internal server error.
 func respondWithInternalError(c *gin.Context, err error) {
