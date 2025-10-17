fd0db98626c567493cf717b279b2f6d1be4f9b5c
mikehquan19 <hoangphucquan19@gmail.com>
2025-10-17 10:10:33 -0500

    Set up for graphql

---
+++ graphql/configs/env.go
@@ -0,0 +1,63 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"log"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+// Get the connection port (if any) from environment
+func GetPortString() string {
+	portNumber, exist := os.LookupEnv("PORT")
+	if !exist {
+		portNumber = "8080"
+	}
+
+	portString := fmt.Sprintf(":%s", portNumber)
+
+	return portString
+}
+
+// Get the connection string of the MongoDB from environment
+func GetEnvMongoURI() string {
+	uri, exist := os.LookupEnv("MONGODB_URI")
+	if !exist {
+		log.Fatalf("Error loading 'MONGODB_URI' from the .env file")
+	}
+
+	return uri
+}
+
+// Get the netID and password from env
+func GetEnvLogin() (netID string, password string) {
+	netID, exist := os.LookupEnv("LOGIN_NETID")
+	if !exist {
+		log.Fatalf("Error loading 'LOGIN_NETID' from the .env file")
+	}
+	password, exist = os.LookupEnv("LOGIN_PASSWORD")
+	if !exist {
+		log.Fatalf("Error loading 'LOGIN_PASSWORD' from the .env file")
+	}
+
+	return netID, password
+}
+
+func GetEnvLimit() int64 {
+	const defaultLimit int64 = 20
+
+	limitString, exist := os.LookupEnv("LIMIT")
+	if !exist {
+		return defaultLimit
+	}
+
+	limit, err := strconv.ParseInt(limitString, 10, 64)
+	if err != nil {
+		return defaultLimit
+	}
+
+	return limit
+}
