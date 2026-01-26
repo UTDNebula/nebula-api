48fc24b82cab0d652373b41cb1b1c50ac6f5fde0
NathanP9000 <73033177+NathanP9000@users.noreply.github.com>
2026-01-26 15:08:27 +0530

    Removed code mismatches from the swagger and docs files.

--- api/docs/docs.go
+++ api/docs/docs.go
@@ -3139,23 +3139,6 @@ const docTemplate = `{
                 }
             }
         },
-        "schema.APIResponse-array_schema_SectionWithTime": {
-            "type": "object",
-            "properties": {
-                "data": {
-                    "type": "array",
-                    "items": {
-                        "$ref": "#/definitions/schema.SectionWithTime"
-                    }
-                },
-                "message": {
-                    "type": "string"
-                },
-                "status": {
-                    "type": "integer"
-                }
-            }
-        },
         "schema.APIResponse-array_schema_TypedGradeData": {
             "type": "object",
             "properties": {
