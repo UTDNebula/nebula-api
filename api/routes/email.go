47fe494d6b255b81c2eafc0c60ff623c83ac93dd
Justin <justinschwerdtfeger245@gmail.com>
2026-03-01 21:32:32 -0600

    Add basic email route similar to storage.go

--- api/routes/email.go
+++ api/routes/email.go
@@ -1,14 +1,32 @@
 package routes
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/UTDNebula/nebula-api/api/controllers"
+	"github.com/UTDNebula/nebula-api/api/schema"
 )
 
 func EmailRoute(router *gin.Engine) {
+	// Rescrict with password
+	authMiddleware := func(c *gin.Context) {
+		secret := c.GetHeader("x-email-key")
+		expected, exist := os.LookupEnv("EMAIL_ROUTE_KEY")
+		if !exist || secret != expected {
+			c.AbortWithStatusJSON(http.StatusForbidden, schema.APIResponse[string]{Status: http.StatusForbidden, Message: "error", Data: "Forbidden"})
+			return
+		}
+		c.Next()
+	}
+
 	// All routes related to email come here
 	emailGroup := router.Group("/email")
+
+	// Use auth
+	emailGroup.Use(authMiddleware)
 
 	emailGroup.OPTIONS("", controllers.Preflight)
 	emailGroup.POST("/send", controllers.SendEmail)
