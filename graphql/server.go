587cdfe66ac85c6e518b955d00234d04831454cd
Kim Bui <kimbuiktb@gmail.com>
2026-09-22 00:19:57 -0500

    Add dataloader middleware to graph server

--- graphql/server.go
+++ graphql/server.go
@@ -42,8 +42,11 @@ func main() {
 		Cache: lru.New[string](100),
 	})
 
+	// Wraps the query handler with middleware for dataloaders
+	graphHandler := loaders.Middleware(srv)
+
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", graphHandler)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
